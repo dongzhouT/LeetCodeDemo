8656ee187ded9cff09e30f8611fff6d6cd3c0a38
tdzh_acer <taodongzhou@126.com>
2020-06-18 22:16:57 +0800

    1028. 从先序遍历还原二叉树

---
+++ src/com/tao/leetcode/daka/Q1028.java
@@ -0,0 +1,61 @@
+package com.tao.leetcode.daka;
+
+
+import com.google.gson.Gson;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 1028. 从先序遍历还原二叉树
+ */
+public class Q1028 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode recoverFromPreorder(String S) {
+        Deque<TreeNode> path = new LinkedList<TreeNode>();
+        int pos = 0;
+        while (pos < S.length()) {
+            int level = 0;
+            while (pos < S.length() && S.charAt(pos) == '-') {
+                level++;
+                pos++;
+            }
+            int value = 0;
+            while (pos < S.length() && Character.isDigit(S.charAt(pos))) {
+                value = value * 10 + (S.charAt(pos) - '0');
+                pos++;
+            }
+            TreeNode node = new TreeNode(value);
+            if (level == path.size()) {
+                if (!path.isEmpty()) {
+                    path.peek().left = node;
+                }
+            } else {
+                while (level != path.size()) {
+                    path.pop();
+                }
+                path.peek().right = node;
+            }
+            path.push(node);
+        }
+        while (path.size() > 1) {
+            path.pop();
+        }
+        return path.peek();
+    }
+
+    public static void main(String[] args) {
+        Q1028 ff = new Q1028();
+        String data = "1-2--3--4-5--6--7";
+        System.out.println(new Gson().toJson(ff.recoverFromPreorder(data)));
+    }
+}
