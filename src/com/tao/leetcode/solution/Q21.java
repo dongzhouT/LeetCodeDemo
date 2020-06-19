477fd1e39e9a40944029a7b4917ec39e2bf3b06d
dongzhouT <taodongzhou@126.com>
2020-06-19 12:02:57 +0800

    21. 合并两个有序链表

---
+++ src/com/tao/leetcode/solution/Q21.java
@@ -0,0 +1,68 @@
+package com.tao.leetcode.solution;
+
+import com.google.gson.Gson;
+
+/**
+ * 21. 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 示例：
+ * <p>
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Q21 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode l3 = new ListNode(0);
+        ListNode head = l3;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                l3.next = l1;
+                l1 = l1.next;
+            } else {
+                l3.next = l2;
+                l2 = l2.next;
+            }
+            l3 = l3.next;
+
+        }
+        l3.next = l1 == null ? l2 : l1;
+        return head.next;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(3);
+        ListNode node6 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node4.next = node5;
+        node5.next = node6;
+
+        System.out.println(new Gson().toJson(new Q21().mergeTwoLists(node1, node4)));
+    }
+}
