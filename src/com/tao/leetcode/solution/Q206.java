2f4e8ac28597a1703d1535c971087a2dfa2b93fa
dongzhouT <taodongzhou@126.com>
2020-06-19 16:04:30 +0800

    206. 反转链表

---
+++ src/com/tao/leetcode/solution/Q206.java
@@ -0,0 +1,43 @@
+package com.tao.leetcode.solution;
+
+import com.google.gson.Gson;
+
+/**
+ * 206. 反转链表
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
+public class Q206 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+    public ListNode reverseListIteration(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        ListNode data = ListNode.buildListNode(new int[]{1, 2, 3, 4, 5});
+        System.out.println(new Gson().toJson(new Q206().reverseListIteration(data)));
+    }
+}
