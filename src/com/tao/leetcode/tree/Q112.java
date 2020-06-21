dd7dce7ad85f5497a8988630b004946befd5619b
tdzh_acer <taodongzhou@126.com>
2020-06-21 23:30:45 +0800

    112. 路径总和 递归

---
+++ src/com/tao/leetcode/tree/Q112.java
@@ -0,0 +1,27 @@
+package com.tao.leetcode.tree;
+
+import com.tao.leetcode.utils.TreeNode;
+
+/**
+ * 112. 路径总和
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ * <p>
+ * 说明: 叶子节点是指没有子节点的节点。
+ */
+public class Q112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        sum -= root.val;
+        if (root.left == null && root.right == null) {
+            return sum == 0;
+        }
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(new Q112().hasPathSum());
+    }
+
+}
